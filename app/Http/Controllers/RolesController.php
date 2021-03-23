bd22f0a2a56a67e0e9e9f549ef5e9b176a46b334
calebbala <calebbala15@gmail.com>
2021-03-23 07:05:21 +0000

    Finished with the roles

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
-    //
+   public function index()
+   {
+       $this->isAdmin();
+
+        $query = Role::all();
+        return RoleResource::collection($query);
+   }
+
+   public function store(Request $request)
+   {
+       $this->isAdmin();
+
+       $this->validate($request, [
+           'role' => 'required'
+       ]);
+       $query = Role::create([
+           'role' => $request->role
+       ]);
+
+       return RoleResource::collection($query);
+
+   }
+
+   public function isAdmin()
+   {
+    if (!auth()->user()->isAdmin) {
+        return response(['message' => "Unathourized"], 401);
+    }
+   }
 }
